7fcdc675f1e6ee13d54d4ed5eda5be546dd46db6
hsssshar <hsssshar@WPG02VBMD>
2022-10-17 11:06:35 +0530

    Added Rest Controller with Two Students

---
+++ spring-using-jenkis/src/main/java/com/example/demo/SpringUsingJenkisApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUsingJenkisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUsingJenkisApplication.class, args);
+	}
+
+}
